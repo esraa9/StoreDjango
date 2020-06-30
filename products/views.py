3e1719e7c0e5ac3c9592e40367c5383a74059899
esraa <esraa.alnasri@yahoo.com>
2020-06-30 20:45:46 +0200

    formatter  views.py

--- products/views.py
+++ products/views.py
@@ -5,10 +5,9 @@ from django.utils import timezone
 
 # Create your views here.
 def say_hi(request, name):
-
-
     return render(request, 'say-hi.html', {'name': name})
+
 
 def show_time(request):
     time = timezone.now()
-    return render(request,'show-time.html',{'time':time})
+    return render(request, 'show-time.html', {'time': time})
