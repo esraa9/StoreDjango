4b7f36b5a5a9797a90a68b1f28ac55cc2e4121c4
esraa <esraa.alnasri@yahoo.com>
2020-07-01 14:04:49 +0200

    edit in description length

--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class product(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=300, blank=True, null=True)
+    description = models.CharField(max_length=500)
 
 
     def __str__(self):
